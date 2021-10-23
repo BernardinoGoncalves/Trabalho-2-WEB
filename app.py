68b656e316b53fa7467d3756255318f7cc6eb004
bernardino_goncalves <bernardo7293@gmail.com>
2021-10-23 19:30:06 -0300

    commit final gravação do video

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import json
+
 import requests
 from flask import Flask, render_template, request, redirect
 
-
 dicUsers = {}
 dicUsers['bernardo@hotmail.com'] = ('Bernardino', '123', 'bernardo@hotmail.com')
-listFav = ['https://myanimelist.net/anime/20/Naruto']
+listFav = {}
 
 app = Flask(__name__)
 
@@ -19,11 +20,10 @@ def login():  # put application's code here
     user = request.form['user']
     password = request.form['password']
     res = dicUsers.get(user, ('null', '-1'))
-    if len(res) < 2:
+    if len(res) <= 2:
         return redirect('/')
     if res[2] == user and res[1] == password:
-        return redirect('/home2')
-        #return render_template('home.html')
+        return redirect('/home')
     return redirect('/')
 
 
@@ -40,26 +40,54 @@ def register():  # put application's code here
     password2 = request.form['password2']
 
     res = dicUsers.get(email, ('null', '-1'))
-    if len(res) > 2: #ja existe esse usuário
+    if len(res) > 2:  # ja existe esse usuário
         return redirect('/registration')
-    if password1 != password2 or name == '': #senhas não são iguais ou nome vazio
+    if password1 != password2 or name == '':  # senhas não são iguais ou nome vazio
         return redirect('/registration')
 
     dicUsers[email] = (name, password1, email)
-    return redirect('/home2')
+    return redirect('/home')
 
 
-@app.route('/home2')
+@app.route('/home')
 def home():  # put application's code here
-
+    search_anime = []
     try:
         text = request.args.get('search')
         text = text.lower().replace(' ', '%20')
         url = 'https://api.jikan.moe/v3/search/anime?q=' + text
-        search_anime = requests.get(url).json()['results']
+        search = requests.get(url).json()['results']
+        for s in search:
+            if s['type'] == 'TV' or s['type'] == 'anime':
+                search_anime.append(s)
     except:
         search_anime = []
-    return render_template('home.html', result=search_anime, fav=listFav)
+    return render_template('home.html', result=search_anime, favorito=listFav)
+
+
+@app.route('/anime')
+def anime():  # put application's code here
+    ep = request.args.get('anime')
+    url = 'https://api.jikan.moe/v3/anime/' + ep
+    anime = requests.get(url).json()
+    image = anime['image_url']
+    title = anime['title']
+    synopsis = anime['synopsis']
+    url = url + '/episodes'
+    eps = requests.get(url).json()['episodes']
+    print(anime)
+    return render_template('animes.html', image=image, title=title, synopsis=synopsis, eps=eps)
+
+
+@app.route('/fav', methods=['POST'])
+def fav():
+    j = request.data
+    favorite = json.loads(j)
+    if not listFav.__contains__(int(favorite['id'])):
+        listFav[int(favorite['id'])] = favorite
+    else:
+        listFav.pop(int(favorite['id']))
+    return '200'
 
 
 if __name__ == '__main__':
